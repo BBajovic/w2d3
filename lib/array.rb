86e9a6c147a44d5a1259e3efe42c803e54bb880e
Bojan Bajovic <Bojan.Bajovic@gmail.com>
2018-08-01 13:55:29 -0400

    Finish basic arrays

--- lib/array.rb
+++ lib/array.rb
@@ -6,4 +6,24 @@ class Array
     end
     result
   end
+
+  def two_sum
+    result = []
+    self.length.times do |i|
+      (i+1..self.length-1).each do |j|
+        result << [i,j] if self[i] + self[j] == 0
+      end
+    end
+    result
+  end
+
+  def my_transpose
+    result = Array.new(self[0].length) {[]}
+    self.each do |row|
+      row.each_index do |idx|
+        result[idx] << row[idx]
+      end
+    end
+    result
+  end
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -2,8 +2,48 @@ require 'rspec'
 require 'array'
 
 describe '#my_uniq' do
+  let(:a) {[1,2,1,3,3]}
   it 'should remove dups' do
-    a = [1, 2, 1, 3, 3]
     expect(a.my_uniq).to eq([1, 2, 3])
+  end
+
+  it 'should not mutate original array' do
+    a.my_uniq
+    expect(a).to eq(a)
+  end
+end
+
+describe '#two_sum' do
+  let(:a) {[-1, 0, 2, -2, 1]}
+  let(:b) {[-1, 0, 0, 2, -2, 1]}
+  it 'should return paired index with sum of 0' do
+    expect(a.two_sum).to eq([[0,4], [2,3]])
+  end
+
+  it 'should return smaller first elements first' do
+    expect(b.two_sum).to eq([[0,5], [1,2], [3,4]])
+  end
+end
+
+describe '#my_transpose' do
+  let(:a) {[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ]}
+  let(:b) {[
+    [0, 1, 2],
+    [3, 4, 5]
+  ]}
+
+  it 'should transpose a 2D array' do
+    expect(a.my_transpose).to eq([
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8]])
+  end
+
+  it 'should work to transpose rectangle array' do
+    expect(b.my_transpose).to eq([[0, 3], [1, 4], [2, 5]])
   end
 end
